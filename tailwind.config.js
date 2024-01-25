1515678923ab89638692c44b430ccb06bf22c049
Nagaprasadvr <nagaprasadvr246@gmail.com>
2024-01-25 17:24:05 +0530

    disable tailwind preflight so the injected styles dont conflict with mui styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: 'jit',
-  purge: ['./src/**/*.{js,jsx,ts,tsx}'],  // adjust as necessary based on your project structure
+  mode: "jit",
+  corePlugins: {
+    preflight: false,
+  },
+  purge: ["./src/**/*.{js,jsx,ts,tsx}"], // adjust as necessary based on your project structure
 
   // ... other Tailwind config options
-}
+};
